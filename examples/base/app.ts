ead207862a743bc1ac188a59b3eb6ba8dfb64006
Vimo <929992114@qq.com>
2019-09-05 07:07:30 +0800

    test simple get

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -1,12 +1,12 @@
 import axios from '../../src/index'
 
-// axios({
-//   method: 'get',
-//   url: '/base/get',
-//   params: {
-//     foo: ['bar', 'baz']
-//   }
-// })
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    foo: ['bar', 'baz']
+  }
+})
 //
 // axios({
 //   method: 'get',
@@ -100,25 +100,25 @@ import axios from '../../src/index'
 //   data: searchParams
 // })
 
-axios({
-  method: 'post',
-  url: '/base/post',
-  data: {
-    a: 1,
-    b: 2
-  }
-}).then((res) => {
-  console.log(res)
-})
-
-axios({
-  method: 'post',
-  url: '/base/post',
-  responseType: 'json',
-  data: {
-    a: 3,
-    b: 4
-  }
-}).then((res) => {
-  console.log(res)
-})
+// axios({
+//   method: 'post',
+//   url: '/base/post',
+//   data: {
+//     a: 1,
+//     b: 2
+//   }
+// }).then((res) => {
+//   console.log(res)
+// })
+//
+// axios({
+//   method: 'post',
+//   url: '/base/post',
+//   responseType: 'json',
+//   data: {
+//     a: 3,
+//     b: 4
+//   }
+// }).then((res) => {
+//   console.log(res)
+// })
