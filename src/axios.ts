d33c1745e6b49f8c12cb5a9905d62d7b34fe42a5
Vimo <929992114@qq.com>
2019-09-09 04:22:13 +0800

    axios 工厂模式 创建, & 代码结构优化

--- src/axios.ts
+++ src/axios.ts
@@ -1,38 +1,15 @@
-import {AxiosPromise, AxiosRequestConfig, AxiosResponse} from './types';
-import xhr from './xhr';
-import {buildURL} from './utils/url';
-import {transformRequest, transformResponse} from './utils/data';
-import {processHeaders} from './utils/headers';
+import {AxiosInstance} from './types';
+import Axios from './core/Axios';
+import {extend} from './utils';
 
-
-const axios = (config: AxiosRequestConfig): AxiosPromise => {
-    processConfig(config);
-    return xhr(config).then(resp => transformResponseData(resp));
+const createInstance = (): AxiosInstance => {
+    const context = new Axios();
+    const instance  = Axios.prototype.request.bind(context);
+    // console.log(instance, context);
+    extend(instance, context);
+    return instance;
 };
 
-const processConfig = (config: AxiosRequestConfig): void => {
-    config.url = transformURL(config);
-    config.headers = transformHeaders(config);
-    config.data = transformRequestData(config);
-};
-
-const transformURL = (config: AxiosRequestConfig): string => {
-    const {url, params} = config;
-    return buildURL(url, params);
-};
-
-const transformRequestData = (config: AxiosRequestConfig): any => {
-    return transformRequest(config.data);
-};
-
-const transformHeaders = (config: AxiosRequestConfig): any => {
-    const {headers = {}, data} = config;
-    return processHeaders(headers, data);
-};
-
-const transformResponseData = (resp: AxiosResponse): AxiosResponse => {
-    resp.data = transformResponse(resp.data);
-    return resp;
-};
+const axios = createInstance();
 
 export default axios;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,3 +5,10 @@ export const isDate = (val: any): val is Date => toString.call(val) === '[object
 export const isObject = (val: any): val is Object => val !== null && typeof val === 'object';
 
 export const isPlainObject = (val: any): val is object => toString.call(val) === '[object Object]';
+
+export function extend<T, U>(to: T, from: U): T & U {
+    for (const key in from) {
+        (to as T & U)[key] = from[key] as any
+    }
+    return to as T & U
+}
