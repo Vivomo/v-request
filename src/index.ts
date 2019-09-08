41a87d0580a5524c4bcc9d4318ae60a2a04a5f19
Vimo <929992114@qq.com>
2019-09-08 11:38:49 +0800

    axios url 处理

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import {AxiosRequestConfig} from './types';
 import xhr from './xhr';
+import {buildURL} from './utils/url';
 
 
-function axios(config: AxiosRequestConfig) {
+const axios = (config: AxiosRequestConfig) => {
+    processConfig(config);
     xhr(config);
-}
+};
+
+const processConfig = (config: AxiosRequestConfig): void => {
+    config.url = transformURL(config);
+};
+
+const transformURL = (config: AxiosRequestConfig): string => {
+    const {url, params} = config;
+    return buildURL(url, params);
+};
 
 export default axios;
